7d6451a31f6e0f30cb6927b595955efe36beccac
PisoDev77 <pisodev0427@gmail.com>
2022-12-22 14:24:46 +0900

    ss

--- api/product.js
+++ api/product.js
@@ -14,7 +14,19 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/testHtml", async (req, res) => {
+router.get("/test1", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "GET SUCESS",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("SERVER ERROR");
+  }
+});
+
+router.get("/test2", async (req, res) => {
   try {
     res.sendFile(path.join(__dirname, "/public/index.html"));
   } catch (err) {
