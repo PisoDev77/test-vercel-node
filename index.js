831808e264ed0c3b73955edc9dd8132ba2e687c6
PisoDev77 <pisodev0427@gmail.com>
2022-12-22 14:30:06 +0900

    ss

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const product = require("./api/product");
 
 app.use(express.json());
-app.use(express.static("public"));
 app.use("/api/product", product);
 
 const PORT = 3000;
